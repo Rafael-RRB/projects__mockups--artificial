cb0ded3d6f85063cad4ed2c644d4c9e7b54488bc
Rafael-RRB <105953150+Rafael-RRB@users.noreply.github.com>
2023-08-31 21:30:17 -0300

    Minor Banner changes. Tools page CSS almost done. I think there is something missing with my Desktop CSS...

--- src/components/ContentSection/ContentSection.jsx
+++ src/components/ContentSection/ContentSection.jsx
@@ -3,31 +3,48 @@ import "./ContentSection.css";
 function ContentSection(props) {
   const HeadingLevel = `h${props.object.headingLevel}`;
   const identifier = props.object.identifier;
+  const modifier = props.object.modifier === undefined ? '' : `--${props.object.modifier}`;
   const title = props.object.title;
   const text = props.object.text;
   const src = props.object.src;
   const alt = props.object.alt;
+  
   let pictureArray = [];
+
+  // "ContentSection" wasn't as reusable as I thought, when I started adding Desktop CSS...
+  const classes = {
+    sec: `content__images${modifier !== undefined ? ` content__images${modifier}` : ''}`,
+    secPic: `content__image${modifier !== undefined ? ` content__image${modifier}` : ''}`,
+    secPicSrc: `image__decoration${modifier !== undefined ? ` image__decoration${modifier}` : ''}`,
+    secPicImg: `image__decoration${modifier !== undefined ? ` image__decoration${modifier}` : ''}`,
+    art: `articles__content${modifier !== undefined ? ` articles__content${modifier}` : ''}`,
+    artSec: `content__text${modifier !== undefined ? ` content__text${modifier}` : ''}`,
+    artHdg: `text__heading${modifier !== undefined ? ` text__heading${modifier}` : ''}`,
+    artTxt: `text__paragraph${modifier !== undefined ? ` text__paragraph${modifier}` : ''}`,
+  }
 
   if(src !== undefined) {
     src.forEach((currentSource, index) => pictureArray.push(
       (
-        <picture className="content__image" key={`picture-${identifier}-${index}`}>
-          <source className='image__decoration' media='(max-width: 280px)' srcSet={`${currentSource}--min.webp`} />
-          <source className='image__decoration' media='(max-width: 540px)' srcSet={`${currentSource}--min.webp`} />
-          <img className='image__decoration' src={`${currentSource}--min.webp`} alt={`${alt}`} />
+        <picture className={`${classes.secPic}`} key={`picture-${identifier}-${index}`}>
+          <source media='(max-width: 280px)' srcSet={`${currentSource}--min.webp`} />
+          <source media='(max-width: 540px)' srcSet={`${currentSource}--min.webp`} />
+          <img className={`${classes.secPicImg}`} src={`${currentSource}--min.webp`} alt={`${alt}`} />
         </picture>
       )
     ));
   }
 
-  return(
-    <article className="articles__content">
-      {pictureArray}
-      
-      <HeadingLevel className='content__heading' >{title}</HeadingLevel>
+  console.log('pictureArray length ? ' + pictureArray.length);
 
-      <p className="content__text">{text}</p>
+  return(
+    <article className={`${classes.art}`}>
+      {pictureArray.length > 1 ? <section className={`${classes.sec}`}>{pictureArray}</section> : pictureArray}
+      
+      <section className={`${classes.artSec}`}>
+        <HeadingLevel className={`${classes.artHdg}`} >{title}</HeadingLevel>
+        <p className={`${classes.artTxt}`} >{text}</p>
+      </section>
     </article>
   );
 }
--- src/pages/Tools/Tools.jsx
+++ src/pages/Tools/Tools.jsx
@@ -1,10 +1,12 @@
-import './Tools.css';
 import Banner from '../../components/Banner/Banner.jsx';
 import bannerSmall from '../../assets/img/banner/banner__tools--min.webp';
 import bannerDefault from '../../assets/img/banner/banner__tools--default.webp';
 import bannerLarge from '../../assets/img/banner/banner__tools--max.webp';
 import ContentSection from '../../components/ContentSection/ContentSection.jsx';
+import './Tools.css';
 // ../../assets/img/banner/banner__tools--min.webp
+
+const page = 'tools';
 
 const bannerList = {
   bannerSmall,
@@ -16,12 +18,14 @@ const bannerList = {
 const toolsIntro = {
   headingLevel: '1',
   identifier: 'intro',
+  modifier: page,
   title: 'Nossas ferramentas',
   text: 'Obrigado pelo seu interesse em como nós criamos nossas artes! Cada imagem foi cuidadosamente criada, utilizando quatro passos:',
 }
 const toolsConcept = {
   headingLevel: '2',
   identifier: 'concept',
+  modifier: page,
   title: 'Conceituação',
   text: 'Nós usamos uma combinação de Imagens da Web, ChatGPT, DALL-E Mini e Crayon para desenvolver a nossa ideia inicial.',
   src: ['/static/pages/tools/tools__concept'],
@@ -30,6 +34,7 @@ const toolsConcept = {
 const toolsGeneration = {
   headingLevel: '2',
   identifier: 'generation',
+  modifier: page,
   title: 'Geração',
   text: 'Nós usamos o DALL-E 2 e outros sites, como Simplified. Alguns prompts necessitaram mais de cem gerações de imagem para obter resultados aceitáveis.',
   src: ['/static/pages/tools/tools__generation'],
@@ -38,6 +43,7 @@ const toolsGeneration = {
 const toolsInpainting = {
   headingLevel: '2',
   identifier: 'inpainting',
+  modifier: page,
   title: 'Inpainting',
   text: 'Em alguns casos, as imagens geradas possuaim boa qualidade, mas eram aruinadas por pequenos defeitos, principalmente em mãos e olhos. Para corrigir isso, utilizamos o site getimg.ai para gerar novamente partes da imagem (áreas pretas, acima) para corrigir esses defeitos. Em apenas um caso, foi necessário corrigir o erro manualmente através do GIMP.',
   src: ['/static/pages/tools/tools__inpainting'],
@@ -46,6 +52,7 @@ const toolsInpainting = {
 const toolsUncropping = {
   headingLevel: '2',
   identifier: 'uncropping',
+  modifier: page,
   title: 'Uncropping',
   text: 'Similar ao problema de defeitos, algumas imagens foram geradas perto demais, afetando a composição da imagem ou removendo detalhes importantes. Para resolver esse problema, o site "neural.love" foi utilizado, gerando as partes que faltavam na imagem original. Note na imagem acima o pequeno erro na geração: voltamos ao inpainting.',
   src: ['/static/pages/tools/tools__uncropping'],
@@ -54,6 +61,7 @@ const toolsUncropping = {
 const toolsOutpainting = {
   headingLevel: '2',
   identifier: 'outpainting',
+  modifier: page,
   title: 'Outpainting',
   text: 'Apenas utilizado para geração dos banners de certas páginas nossas. Todas as imagens geradas possuiam resolução de 1024x1024 pixels, e o DALL-E 2 foi utilizado para expandir horizontalmente essas imagens, que posteriormente foram cortadas para atingir um Aspect Ratio de 16:9.',
   src: [
@@ -68,9 +76,9 @@ const toolsOutpainting = {
 function Tools(props) {
   return (
     <main className="main">
-      <Banner page={'tools'} bannerList={bannerList} alt={'desenho de cinco cachorros ciêntistas.'} />
+      <Banner page={page} bannerList={bannerList} alt={'desenho de cinco cachorros ciêntistas.'} />
 
-      <section className="articles">
+      <section className="articles articles--tools">
         <ContentSection object={toolsIntro} />
         <ContentSection object={toolsConcept} />
         <ContentSection object={toolsGeneration} />
