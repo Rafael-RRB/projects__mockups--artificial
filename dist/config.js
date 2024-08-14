be38bcb9584e65e856a9efad434d33c58303a946
Rafael-RRB <105953150+Rafael-RRB@users.noreply.github.com>
2024-08-14 18:57:13 -0300

    Attempt to fix GitHub pages...

---
+++ dist/config.js
@@ -0,0 +1,9 @@
+// For variables that will be used in multiple places
+// Social media links
+export const socialLinks = {
+  alura: 'https://cursos.alura.com.br/user/rafaelrb',
+  linkedin: 'https://www.linkedin.com/in/rafaelrrb/',
+  github: 'https://github.com/Rafael-RRB',
+  whatsapp: 'https://www.linkedin.com/in/rafaelrrb/',
+  telegram: 'https://www.linkedin.com/in/rafaelrrb/'
+}
